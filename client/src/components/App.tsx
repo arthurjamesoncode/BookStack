3cd1a1f2937d926631a4693439df1e7cae7e2db3
arthurjamesoncode <arthurj17@gmail.com>
2023-11-08 16:42:25 +0000

    init client and styled navbar

---
+++ client/src/components/App.tsx
@@ -0,0 +1,8 @@
+import '../styles/App.css';
+import { Navbar } from './Navbar';
+
+function App() {
+  return <><Navbar/></>;
+}
+
+export default App;
---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import logoIcon from '../assets/book-stack-icon.png';
+
+export function Navbar() {
+  return (
+    <header>
+      <div className='heading-container'>
+        <h1 className='heading'>BookStack</h1>
+        <img className="logo-icon" src={logoIcon} />
+      </div>
+      <div className='menu-icon'>
+        <div className='menu-line' />
+        <div className='menu-line' />
+        <div className='menu-line' />
+      </div>
+    </header>
+  );
+}
