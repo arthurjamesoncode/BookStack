04254b21098e7d51eb78231371693dae19ff4675
arthurjamesoncode <arthurj17@gmail.com>
2023-11-09 09:47:08 +0000

    chore: styling+gitignore

--- client/src/components/BookPreview.tsx
+++ client/src/components/BookPreview.tsx
@@ -4,23 +4,28 @@ type BookPreviewProps = {
   book: Book;
 };
 
-export default function BookPreview({ book }: BookPreviewProps) {
+export default function BookPreview({ book}: BookPreviewProps) {
+
   return (
-    
-      <div className='book-container grid'>
-        <img className='large-cover-img' alt={`The cover of ${book.title}`} />
-        <div className='book-info'>
-          <h3>{book.title}</h3>
-          <h4>by {book.author}</h4>
-          <div className='description'>
-            <h5>Description:</h5>
-            <p>
-              {book.description != null && book.description!.length > 50
-                ? `${book.description?.substring(0, 47)}...`
-                : book.description}
-            </p>
-          </div>
+    <div className='book-container grid'>
+      <img className='large-cover-img' alt={`The cover of ${book.title}`} />
+      <div className='book-info'>
+        <h3>{book.title}</h3>
+        <h4>by {book.author}</h4>
+        <div className='description'>
+          <h5>Description:</h5>
+          <p>
+            {book.description != null && book.description!.length > 50
+              ? `${book.description?.substring(0, 47)}...`
+              : book.description}
+          </p>
+        </div>
+        <div className='action-buttons'>
+          <button>View</button>
+          <button>Edit</button>
+          <button >Delete</button>
         </div>
       </div>
+    </div>
   );
 }
