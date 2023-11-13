8d2edb8535d9880768e4c4fc3d9d119cb0ca4ab4
arthurjamesoncode <arthurj17@gmail.com>
2023-11-13 18:32:35 +0000

    feat: seperate search result into pages

--- client/src/components/Search/Search.tsx
+++ client/src/components/Search/Search.tsx
@@ -4,8 +4,13 @@ import { SearchResponse, SearchResult } from '../../utils/types';
 import { searchFor } from '../../services/OpenLibrary';
 import SearchResultsPage from './SearchResultPage/SearchResultPage';
 
+import leftArrow from '../../assets/arrow-left.svg';
+import rightArrow from '../../assets/arrow-right.svg';
+
 export default function Search() {
   const [results, setResults] = useState<SearchResult[]>([]);
+  const [resultCount, setResultCount] = useState(0);
+  const [index, setIndex] = useState(0);
 
   async function onSearch(values: Record<string, string | number>) {
     const { searchText } = values as { searchText: string };
@@ -13,12 +18,36 @@ export default function Search() {
     const responseData: SearchResponse = await searchFor(searchText);
 
     setResults(responseData.docs);
+    setResultCount(responseData.docs.length);
   }
 
+  function changeIndex(diff: number) {
+    setIndex((prev) => prev + diff);
+  }
+
+  const pageTotal = Math.floor(resultCount / 10) + 1;
+  const min = (index + 1) * 10 - 10;
+  const max = (index + 1) * 10;
   return (
     <div className='search-container'>
       <SearchForm onSearch={onSearch} />
-      <SearchResultsPage page={results} />
+      <SearchResultsPage page={results.slice(min, max)} />
+      <div>
+        Page {index + 1} out of {pageTotal}
+        <img
+          onClick={() => {
+            if (index > 0) {
+              changeIndex(-1);
+            }
+          }}
+          src={leftArrow}
+        />
+        <img onClick={() => {
+            if (index + 1 < pageTotal) {
+              changeIndex(1);
+            }
+          }} src={rightArrow} />
+      </div>
     </div>
   );
 }
